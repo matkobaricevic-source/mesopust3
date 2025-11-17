0a73e38f86b18872b11fd6413ae1362ef2634411
matkobaricevic-source <matko.baricevic@gmail.com>
2025-11-17 15:12:56 +0100

    Updated app.json

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
+import Constants from 'expo-constants';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl || '';
+const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
